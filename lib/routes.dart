65023c8bc9a44d55be571ff72869ef367cdb8365
LordChristian <lordchristian.sugbodoc@gmail.com>
2023-11-14 10:11:58 +0800

    SM1 - Finalized Initial Patient Profile Screen
    * Modified Routes added Patient Profile Route
    
    * Modified Login Screen Added Functionality to Navigate to Patient Profile Screen
    
    * Modified NavBar Widget Added Settings
    
    * Added Patient Profile to Library/Screens

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_doctor_ui/screens/dashboard.dart';
 import 'package:flutter_doctor_ui/screens/login.dart';
+import 'package:flutter_doctor_ui/screens/patient_profile.dart';
 import 'package:flutter_doctor_ui/screens/registration.dart';
 
 final Map<String, WidgetBuilder> routes = {
   Login.routeName: (BuildContext context) => Login(),
   Registration.routeName: (BuildContext context) => Registration(),
   Dashboard.routeName: (BuildContext context) => Dashboard(),
+  PatientProfile.routeName: (BuildContext context) => PatientProfile(),
 };
